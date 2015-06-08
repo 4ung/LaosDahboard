33f3076b8211ceff44945e8cde10cb2f89dd8ef6
winhtaikaung <winhtaikaung28@hotmail.com>
2015-06-08 12:02:14 +0630

    MDG-6 Dashboard Completed

--- src/scripts/fragments/MDG5.js
+++ src/scripts/fragments/MDG5.js
@@ -43,7 +43,7 @@ $(function(){
 			"StringTitle":"Lao PDR",
 			"StringSubtitle":"Source hmis.gov.la",
 			"ArrayXcategories":categories_Chart2_MDG5,
-			"ArrayColors":['#00a2c5','#000'],
+			"ArrayColors":['#00a2c5','#000','#31ba64','#000'],
 			"StringYtitle":"",
 			"BooleanLegend":true,
 			"StringTooltip":tooltip_Chart2,
@@ -64,7 +64,7 @@ $(function(){
     var tooltip_Chart3='<b>{point.y:.1f} </b>';
 	var chart3_MDG5={
 			"StringType":"",
-			"StringTitle":"Maternal Mortality Ratio",
+			"StringTitle":"Maternal Mortality Ratio \n(Deaths per 100,000 live Births)",
 			"StringSubtitle":"Source hmis.gov.la",
 			"ArrayXcategories":categories_Chart3_MDG5,
 			"ArrayColors":['#00a2c5','#000'],
@@ -75,16 +75,59 @@ $(function(){
 	};
 
 
+	var categories_Chart4_MDG5=[
+		'Luang Namatha',
+		'Attapeu',
+		'Vientiene. C',
+		'Sekong',
+		'Odumxay',
+		'Phongsaly',
+		'Huaphanh',
+		'Saravane',
+		'Bokeo',
+		'Champasack',
+		'Khummuane',
+		'Xaysomboun',
+		'Vientine.P',
+		'BoriKhanxay',
+		'Luangprabang',
+		'Xaybury',
+		'XiengKhuang',
+		'Savannakhet'
+
+	];
+	var dataElements_Chart4_MDG5=[{
+									name:'SBA Coverage',
+									data:[23.4,26.3,27.9,29,29.4,29.4,38.4,38.6,42.3,42.7,46.2,48.5,49.8,53.4,54.7,60.2,63.8,68.7]
+								}];
+
+    var tooltip_Chart4='<b>{point.y:.1f} </b>';
+	var chart4_MDG5={
+			"StringType":"column",
+			"StringTitle":"SBA Delivery By Province",
+			"StringSubtitle":"Source hmis.gov.la",
+			"ArrayXcategories":categories_Chart4_MDG5,
+			"ArrayColors":['#00a2c5'],
+			"StringYtitle":"",
+			"BooleanLegend":true,
+			"StringTooltip":tooltip_Chart4,
+			"ArraySeries":dataElements_Chart4_MDG5
+	};
+
+
+
+
+
 	var map1_MDG5={
 		Mapdatapath:"MDG5-map/Mdg5_Map1.json",
-        StringTitleText:"ANC 2014",
+        StringTitleText:"ANC First Visit Coverage",
         BooleanNavigation:true,
         ColorMin:"#000",
         ColorMax:"#232323",
         ArrayChroplethRange:[
                     [0, '#EFEFFF'],
-                    [0.6, '#4444FF'],
-                    [1, '#232323']
+                    [10, '#4444FF'],
+                    [30, '#232323']
                 ],
         BooleanDataLabels:true,
         StringToolTip:"Data Label Tooltip",
@@ -125,14 +168,14 @@ $(function(){
 
 
 	loadmanifest().done(function(data){
-      window.DASHBOARD.MAP_PATH=window.DASHBOARD.BASE_URL+"/"+data.map_path+data.maps.LAOS_DISTRICT;
+      window.DASHBOARD.MAP_PATH=window.DASHBOARD.BASE_URL+"/"+data.map_path+data.maps.LAOS;
       //Call Method from  helper Library
       	helper.loadData(window.DASHBOARD.MAP_PATH).done(function(mapdata){
             mapHelper.mapBuilder("#MAP1_MDG5",map1_MDG5,mapdata);
   		});
     
   	});
-
+/*
   	loadmanifest().done(function(data){
       window.DASHBOARD.MAP_PATH=window.DASHBOARD.BASE_URL+"/"+data.map_path+data.maps.LAOS_DISTRICT;
       //Call Method from  helper Library
@@ -150,10 +193,11 @@ $(function(){
   		});
     
   	});
-
+*/
 	chartHelper.BarChartBuilder("#Chart1_MDG5",chart1_MDG5);
 	chartHelper.BarChartBuilder("#Chart2_MDG5",chart2_MDG5);
 	chartHelper.BarChartBuilder("#Chart3_MDG5",chart3_MDG5);
+	chartHelper.BarChartBuilder("#Chart4_MDG5",chart4_MDG5);
 
 	
 
--- src/scripts/fragments/MDG6.js
+++ src/scripts/fragments/MDG6.js
@@ -1,24 +1,28 @@
 $(function(){
 	
 	var categories_Chart1_MDG6=[
-		'2000','2005','2009','2010','2012','2013'
+		'2009','2012','2014'
 	];
 
 	var dataElements_Chart1_MDG6=[{
-									name:'MDG-HIV Prevalence among general Population',
-									data:[0.1,0.18,0.2,0.25,0.28,0.29]
+									name:'MDG-HIV Prevalence among MSM 15-49 years',
+									data:[0,2.4,1.6]
 								},{
-									name:'Target',
-									data:[1,1,1,1,1,1]
+									name:'MDG-HIV Prevalence among MSM 15-24 (%) years total',
+									data:[0,1.2,2]
+								},
+								{
+									name:'MDG-HIV Prevalence among MSM 15-24 (%) years total',
+									data:[0,0.28,0.29]
 								}];
 	var tooltip_Chart1='<b>{point.y:.1f} </b>';
 
 	var chart1_MDG6={
 			"StringType":"",
-			"StringTitle":"HIV Prevalence Among general Population VS Target",
+			"StringTitle":"HIV Prevalence Among \nGeneral Population and Risk Groups",
 			"StringSubtitle":"Source hmis.gov.la",
 			"ArrayXcategories":categories_Chart1_MDG6,
-			"ArrayColors":['#00a2c5','#000'],
+			"ArrayColors":['#00a2c5','#ffa110','#9b74b6'],
 			"StringYtitle":"",
 			"BooleanLegend":true,
 			"StringTooltip":tooltip_Chart1,
@@ -27,18 +31,22 @@ $(function(){
 
 
 	var categories_Chart2_MDG6=[
-		'2005','2010','2012','2013','2014'
+		'1990','1995','2000','2005','2009','2010','2012','2014'
 	];
 
 	var dataElements_Chart2_MDG6=[{
-									name:'MDG Percantage of Adults and \n \nchildren with advanced HIV infection received',
-									data:[40.8,50.8,55.4,58.2,84]
+									name:'Maleria Incidence (per 1000 pop)',
+									data:[10,12,8.1,3.5,2.8,3.5,2.7,7.1]
+								},
+								{
+									name:'Death Rate Associated with Maleria(per 100,000 pop)',
+									data:[9,14,7.1,3.5,1.4,0.4,0.3,0.6]
 								}];
 	var tooltip_Chart2='<b>{point.y:.1f} </b>';
 
 	var chart2_MDG6={
 			"StringType":"column",
-			"StringTitle":"LAO PDR",
+			"StringTitle":"Maleria Incidence And Deaths",
 			"StringSubtitle":"Source hmis.gov.la",
 			"ArrayXcategories":categories_Chart2_MDG6,
 			"StringYtitle":"",
@@ -48,33 +56,24 @@ $(function(){
 	};
 
 	var categories_Chart3_MDG6=[
-		'Oct 1999 to Sep 2000','Oct 2004 to Sep 2005 ','Oct 2008 to Sep 2009','Oct 2009 to Sep 2010','Oct 2011 to Sep 2012','Oct 2012 to Sep 2013'
+		'2010','2012','2013','2014'
 	];
 
 	var dataElements_Chart3_MDG6=[{
 									name:'MDG HIV prevalence among General',
-									data:[0.03,0.18,0.2,0.25,0.28,0.29]
-								},
-								{
-									name:'Target MDG HIV prevalence among General',
-									data:[0,0,0,0,0,0]
-								},
-								{
-									name:'MDG HIV prevalence among general population',
-									data:[0,1.8,2,1.4,1.2,5]
-								},
-								{
-									name:'Target HIV prevalence among general population',
-									data:[5,5,5,5,5,5]
+									data:[50.8,55.4,58.2,60.6]
+								},{
+									name:'Target',
+									data:[90,90,90,90]
 								}];
 	var tooltip_Chart3='<b>{point.y:.1f} </b>';
 
 	var chart3_MDG6={
 			"StringType":"",
-			"StringTitle":"LAO PDR",
+			"StringTitle":"MDG ARV Trend Annual",
 			"StringSubtitle":"Source hmis.gov.la",
 			"ArrayXcategories":categories_Chart3_MDG6,
-			"ArrayColors":['#00a2c5','#000','#31ba64','#000'],
+			"ArrayColors":['#00a2c5','#000'],
 			"StringYtitle":"",
 			"BooleanLegend":true,
 			"StringTooltip":tooltip_Chart3,
